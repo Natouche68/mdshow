ea62e851d464c1ab36be845ecfcdf3db83e62436
Natouche68 <ngrossheny@gmail.com>
2024-12-01 18:28:09 +0100

    Read Markdown from file

---
+++ example.md
@@ -0,0 +1,13 @@
+# This is a test markdown file
+
+This is a [link](http://example.com)
+
+```go
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello, World!")
+}
+```
--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gomarkdown/markdown"
 )
 
 func main() {
-	md := []byte("# Hello world")
+	md, err := os.ReadFile("example.md")
+	if err != nil {
+		panic(err)
+	}
+
 	html := markdown.ToHTML(md, nil, nil)
 	fmt.Println(string(html))
 }
