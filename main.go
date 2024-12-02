c80e39fa9063f9d90daadde573613f3cceda99d8
Natouche68 <ngrossheny@gmail.com>
2024-12-02 19:05:42 +0100

    Add a logger

--- main.go
+++ main.go
@@ -5,13 +5,16 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/charmbracelet/log"
 	"github.com/gomarkdown/markdown"
 )
 
 func main() {
+	log.SetReportTimestamp(false)
+
 	md, err := os.ReadFile("example.md")
 	if err != nil {
-		panic(err)
+		log.Fatal("Error reading Markdown file", "err", err)
 	}
 
 	html := markdown.ToHTML(md, nil, nil)
@@ -19,11 +22,13 @@ func main() {
 	tmpl, err := template.ParseFiles("templates/index.gohtml")
 
 	if err != nil {
-		panic(err)
+		log.Fatal("Error parsing template", "err", err)
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, template.HTML(string(html)))
 	})
+
+	log.Info("Server started on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
