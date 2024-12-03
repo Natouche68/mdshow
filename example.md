7b8505baf9e7a3412a13d32c41021c5b4eee8223
Natouche68 <ngrossheny@gmail.com>
2024-12-03 18:57:11 +0100

    Change slides and add styles

--- example.md
+++ example.md
@@ -1,6 +1,30 @@
 # This is a test markdown file
 
+A test markdown file for testing the MDShow CLI tool.
+
+---
+
+## H2
+
+### H3
+
+#### H4
+
+##### H5
+
+###### H6
+
+---
+
 This is a [link](http://example.com)
+
+- Item 1
+- Item 2
+- Item 3
+
+---
+
+This is some `Go` code.
 
 ```go
 package main
--- main.go
+++ main.go
@@ -4,20 +4,18 @@ import (
 	"html/template"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/charmbracelet/log"
 	"github.com/gomarkdown/markdown"
 )
 
+type PresentationData struct {
+	Content template.HTML
+}
+
 func main() {
 	log.SetReportTimestamp(false)
-
-	md, err := os.ReadFile("example.md")
-	if err != nil {
-		log.Fatal("Error reading Markdown file", "err", err)
-	}
-
-	html := markdown.ToHTML(md, nil, nil)
 
 	tmpl, err := template.ParseFiles("templates/index.gohtml")
 
@@ -26,7 +24,18 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, template.HTML(string(html)))
+		md, err := os.ReadFile("example.md")
+		if err != nil {
+			log.Fatal("Error reading Markdown file", "err", err)
+		}
+
+		html := string(markdown.ToHTML(md, nil, nil))
+
+		html = strings.ReplaceAll(html, "<hr>", "</div><div class=\"slide\">")
+
+		tmpl.Execute(w, PresentationData{
+			Content: template.HTML(html),
+		})
 	})
 
 	log.Info("Server started on http://localhost:8080")
